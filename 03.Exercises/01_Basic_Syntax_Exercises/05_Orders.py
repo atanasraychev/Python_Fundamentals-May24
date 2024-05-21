586ceb1cd12d816bb8749f788a32be0c9b15bba1
atanasraychev <raychev82@gmail.com>
2024-05-21 09:22:56 +0300

    Syntax_4_Test

--- 03.Exercises/01_Basic_Syntax_Exercises/05_Orders.py
+++ 03.Exercises/01_Basic_Syntax_Exercises/05_Orders.py
@@ -2,16 +2,16 @@ orders_count = int(input())
 
 total_orders_price = 0.00
 
-for i in range(0, orders_count, 1):
-    single_order_price = 0.00
+for orders in range(orders_count):
+
     capsule_price = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
     if capsule_price < 0.01 or capsule_price > 100.0:
         continue
-    days = int(input())
-    if days > 31 or days < 1:
+    elif days not in range(1, 31+1):
         continue
-    capsules_per_day = int(input())
-    if capsules_per_day < 1 or capsules_per_day > 2000:
+    elif capsules_per_day < 1 or capsules_per_day > 2000:
         continue
 
     single_order_price = capsule_price * days * capsules_per_day
